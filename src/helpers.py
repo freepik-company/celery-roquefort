54375fcee4ce33a25fda157c8285d7f09bf091ea
jamuriano <jamuriano@freepik.com>
2025-06-25 13:34:23 +0200

    feat: add helper functions for worker and queue metadata handling
    - Introduced `get_worker_names` to extract worker and hostname from a given name.
    - Added `get_queue_name_from_worker_metadata` to retrieve queue names from worker metadata.
    - Enhanced `format_queue_names` function to improve queue name formatting.

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,5 @@
+from celery.utils import nodesplit  # type: ignore
+
 def format_queue_names(queue_names: list[str]) -> str:
     """Format a list of queue names into a string.
 
@@ -8,3 +10,17 @@ def format_queue_names(queue_names: list[str]) -> str:
         str: _description_
     """
     return ",".join(queue_names)
+
+
+def get_worker_names(name: str) -> tuple[str, str]:
+    workername, hostname = nodesplit(name)
+    return workername, hostname
+
+def get_queue_name_from_worker_metadata(worker: str, metadata: dict) -> str:
+    """Get the queue name from the worker metadata.
+
+    Args:
+        worker (str): The worker name.
+        metadata (dict): The worker metadata.
+    """
+    return get_worker_names(metadata.get(worker, {}).get("queues", []))
