98e3df7b80cfc6413a9483c47d722b1282aed67b
José Antonio Muriano Criado <jamuriano@freepik.com>
2025-06-25 12:15:45 +0200

    feat: add queue control inspection for detect queue linked by workers (#3)
    * feat: enhance Roquefort and HttpServer with graceful shutdown and improved metrics handling
    
    - Added shutdown event to Roquefort for graceful termination of metrics collection.
    - Updated metrics collection to handle socket timeouts and log errors appropriately.
    - Ensured HTTP server starts only once to prevent multiple initializations.
    - Improved logging for better traceability during server and metrics operations.
    
    * refactor: simplify server startup logic in HttpServer class
    
    * feat: add helper function to format queue names
    
    - Introduced a new function `format_queue_names` in `helpers.py` to format a list of queue names into a comma-separated string.
    
    * chore: add development dependency group for code formatting tools
    
    - Introduced a new dependency group for development, including `black` for code formatting.
    
    * feat: add worker metadata for handle label creation like "queue_name"
    
    - Enhanced code formatting for better readability in the Roquefort class.
    - Added a new attribute `_workers_metadata` to store worker queue information.
    - Updated logging messages for consistency and clarity.
    - Improved the handling of worker heartbeats and metrics collection.
    - Ensured proper formatting of queue names using the new helper function.
    
    * style: set codestyle using black

--- src/server/server.py
+++ src/server/server.py
@@ -5,12 +5,18 @@ import time
 from typing import Callable
 from fastapi import FastAPI
 import uvicorn
-from prometheus_client import disable_created_metrics, make_asgi_app, Counter, start_http_server
+from prometheus_client import (
+    disable_created_metrics,
+    make_asgi_app,
+    Counter,
+    start_http_server,
+)
 
 from ..metrics.metrics import MetricService
 
+
 class HttpServer:
-    def __init__(self, host: str = "0.0.0.0", port: int = 8000, registry = None):
+    def __init__(self, host: str = "0.0.0.0", port: int = 8000, registry=None):
         self._host = host
         self._port = port
         self._registry = registry
@@ -20,9 +26,8 @@ class HttpServer:
         start_http_server(addr=self._host, port=self._port, registry=self._registry)
         logging.info(f"HTTP server started on {self._host}:{self._port}")
         await asyncio.sleep(1)
-        
-        
+
+
 if __name__ == "__main__":
     server = HttpServer()
     server.run()
-
